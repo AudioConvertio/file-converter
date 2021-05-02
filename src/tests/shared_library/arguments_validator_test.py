7584ad929a223ba1df1dc2b78f4cf80e60dfe5d4
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-02 16:00:56 -0300

    Updating functionalities
    Now the program works converting every archive is a directory to the required output

--- src/tests/shared_library/arguments_validator_test.py
+++ src/tests/shared_library/arguments_validator_test.py
@@ -1,31 +1,13 @@
 import pytest
-from audio_convertio.shared_library.arguments_validator import load_arguments, file_accessible, LIST_OF_ACCEPTED_FORMATS
+from audio_convertio.shared_library.arguments_validator import dir_accessible
 from audio_convertio.classes.custom_exceptions import *
 
 
 class TestArgumentsValidator(object):
+    def test_dir_accessible_file_is_directory(self):
+        with pytest.raises(IsAFile):
+            dir_accessible('LICENSE')
 
-    def test_load_arguments_valid_return_type(self):
-        args = load_arguments([('-i', 'archive'), ('-o', 'wmv')])
-        assert type(args) is list
-
-    def test_load_arguments_valid_number_of_arguments(self):
-        args = load_arguments([('-i', 'archive'), ('-o', 'wmv')])
-        assert len(args), 2
-
-    def test_load_arguments_valid_input(self):
-        args = load_arguments([('-i', 'archive'), ('-o', 'wmv')])
-        assert args[0] is 'archive'
-        assert args[1] is 'wmv'
-
-    def test_load_arguments_valid_format_type(self):
-        args = load_arguments([('-i', 'archive'), ('-o', 'wmv')])
-        assert args[1] in LIST_OF_ACCEPTED_FORMATS
-
-    def test_file_accessible_file_not_found(self):
-        with pytest.raises(FileNotExists):
-            file_accessible('#')
-
-    def test_file_accessible_file_is_directory(self):
-        with pytest.raises(IsADirectory):
-            file_accessible('classes')
+    def test_dir_accessible_file_not_found(self):
+        with pytest.raises(DirectoryNotExists):
+            dir_accessible('bat')
