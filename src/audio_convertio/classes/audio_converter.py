bc7a26102bc782b54f4902633fcf0cd8d10f9c9e
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-30 13:29:26 -0300

    Adding parallel audio convertion method

--- src/audio_convertio/classes/audio_converter.py
+++ src/audio_convertio/classes/audio_converter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 
+from multiprocessing import Pool, cpu_count
 from pydub import AudioSegment
 
 
@@ -10,34 +11,50 @@ class Audio():
     Args:
         audios_dir (str): the path where the audio is stored
         input_format (str): the format of the files to be converted
+        output_format (str): the file output format
     """
 
-    def __init__(self, audios_dir: str, input_format: str):
+    def __init__(self, audios_dir: str, input_format: str, output_format: str):
         self.audios_dir = audios_dir
         self.input_format = input_format
+        self.output_format = output_format
 
     def __repr__(self) -> str:
         return f"This is my audio_path: {self.audios_dir} and this is my audio input: {self.input_format}"
 
-    def convert_files(self, output_format: str) -> None:
+    def convert_files_sequential(self) -> None:
         """Iterates over every file in the dir with the input_format and
-        converts it to the output_format
-
-        Args:
-            output_format (str): the file output format
+        converts it to the output_format in a sequential way
         """
         for file in os.listdir(self.audios_dir):
             if file.endswith(self.input_format):
-                print(f"Converting {file} to {output_format}")
                 self.convert_file(os.path.join(
-                    self.audios_dir, file), output_format)
+                    self.audios_dir, file), self.output_format)
 
-    def convert_file(self, file_path: str, output_format: str) -> None:
-        """Converts the current audio to a new output format
+    def convert_files_parallel(self) -> None:
+        """Iterates over every file in the dir with the input_format and
+        converts it to the output_format in a parallel way
 
         Args:
             output_format (str): the file output format
         """
+        file_paths = []
+        for file in os.listdir(self.audios_dir):
+            if file.endswith(self.input_format):
+                file_paths.append(os.path.join(
+                    self.audios_dir, file))
+        with Pool(cpu_count()) as p:
+            p.map(self.convert_file, file_paths)
 
-        output = AudioSegment.from_file(file_path, format=output_format)
-        return output.export(file_path.replace(self.input_format, f'{output_format}'), format=output_format)
+    def convert_file(self, file_path: str) -> None:
+        """Converts the current audio to a new output format
+
+        Args:
+            file_path (str): path of the file to be converted
+        """
+        print(
+            f"Converting {os.path.split(file_path)[1]} to {self.output_format}")
+
+        output = AudioSegment.from_file(file_path, format=self.output_format)
+        output.export(file_path.replace(self.input_format,
+                      f'{self.output_format}'), format=self.output_format)
